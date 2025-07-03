7d06e041d33e3d64567f75d283425f3e8962e783
idksarah <sarah.p.ngai@gmail.com>
2025-07-03 04:58:33 -0700

    Type

--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -13,6 +13,7 @@ function Faq() {
             <IYapathon topShit="does this have any prerequisites?" bottomBitch="no. plus, onshape has extensive documentation, so this is an approachable project for someone who hasn't tried cad before"/>
             <IYapathon topShit="do i need to use hackatime to track my time?" bottomBitch="yes. you must log at least 3 hours on the onshape wakatime extension to receive your mouse parts."/>
             <IYapathon topShit="how will i get this printed?" bottomBitch="find someone willing to print (#printing-legion or check the site)"/>
+            <IYapathon topShit="can i import parts?" bottomBitch='yes, but only parts other than the case. eg, the pcb, sensor, etc'/>
             <IYapathon topShit="when does this end?" bottomBitch="7/10/2025 "/>
             <IYapathon topShit="will hack club cover shipping?" bottomBitch="yes"/>
             <IYapathon topShit="what da mouse named?" bottomBitch="dore"/>
