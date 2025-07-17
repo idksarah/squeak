fc101f0676f976ffdbf2de13e6d281310127ef52
idksarah <sarah.p.ngai@gmail.com>
2025-07-16 20:28:06 -0700

    reamde

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # squeak!
+squeak.hackclub.com
 
-contains the website and cad files for an upcoming hack club ysws where you can design a mouse case and receive the pcb and printed case
+contains website and cad files for squeak, a hack club ysws where you can design a mouse case and receive the parts (from bambu's mouse kit) and printed case
+
+onshape files are located in /onshape_imports . download the files and import them to onshape to use them.
 
 # usage
 clone this repo <br>
 npm run dev
+
+![Screenshot](./public/ss.png)
--- src/pages/Faq.tsx
+++ src/pages/Faq.tsx
@@ -11,10 +11,10 @@ function Faq() {
           <p className="h1">faq</p>
           <div className="">
             <IYapathon topShit="does this have any prerequisites?" bottomBitch="no. plus, onshape has extensive documentation and a good community, so this is an approachable project for someone who hasn't tried cad before"/>
-            <IYapathon topShit="do i need to use hackatime to track my time?" bottomBitch="yes. you must log at least 4 hours on the onshape wakatime extension to receive your mouse parts."/>
+            <IYapathon topShit="do i need to use hackatime to track my time?" bottomBitch="yes. you must log at least 3 hours on the onshape wakatime extension to receive your mouse parts."/>
             <IYapathon topShit='how can i make my mouse unique?' bottomBitch='lots of ways! eg, include pocketing, make it ergonomic, or use  a completely different shape. search up mice and get inspo!'></IYapathon>
             <IYapathon topShit="how will i get this printed?" bottomBitch="find someone willing to print (#printing-legion or check the site). we'll provide a $5 shipping grant!"/>
-            <IYapathon topShit="can i import parts to onshape?" bottomBitch='yes, but not parts of the case. eg, you cannot import the bottom or top part of your case, but you can import the pcb, sensor, switch, etc.'/>
+            {/* <IYapathon topShit="can i import parts to onshape?" bottomBitch='yes, but not parts of the case. eg, you cannot import the bottom or top part of your case, but you can import the pcb, sensor, switch, etc.'/> */}
             <IYapathon topShit="when does this end?" bottomBitch="11:59 pm est 7/13/2025 "/>
             <IYapathon topShit="what da mouse named?" bottomBitch="dore"/>
             <IYapathon topShit="i have more questions!" bottomBitch="ask in #squeak"/>
